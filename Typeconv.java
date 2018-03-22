640556e29b569b03fdc3d2902c3096445ab2d97f
Makoto Tanabe <xekid78@outlook.com>
2018-03-22 17:38:17 +0900

    Add files via upload

---
+++ Typeconv.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class Typeconv {
+
+	public static void main(String[] args) {
+		System.out.println("** ケース1 **");
+		double num1 = 3.15;
+		System.out.println(num1);
+		System.out.println((int)num1);
+		System.out.println("");
+
+		System.out.println("** ケース２ **");
+		int num2 = 5;
+		System.out.println(num2);
+		System.out.println((double)num2);
+		System.out.println("");
+
+		System.out.println("** ケース３ **");
+		String text = "123";
+		System.out.println(text);
+		System.out.println(Integer.parseInt(text) * 100);
+		System.out.println(Double.parseDouble(text));
+	}
+
+}
